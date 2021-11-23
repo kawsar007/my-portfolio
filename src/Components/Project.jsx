96bedd044dd48a6b73ee672bdfa63bb3c52c69db
kawsar007 <imkawsar007@gmail.com>
2021-11-24 00:10:05 +0600

    Single project add.

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import covid1 from "./img/covid1.jpg";
-import Starware from "./img/starware.png";
-import landing1 from "./img/landing1.jpg";
-import commerce1  from "./img/commerce1.jpg";
-import netflix1 from "./img/netflix1.jpg";
 import github1 from "./img/github1.jpg";
+import landing1 from "./img/landing1.jpg";
+import SuperBike from "./img/mern.jpg";
+import netflix1 from "./img/netflix1.jpg";
+import Starware from "./img/starware.png";
 
 function Project() {
   const [state] = useState([
@@ -17,69 +17,79 @@ function Project() {
       // text:
       //   "It's amazing app Search Github User.Using React, Hooks, Context Api,  material-ui and Fusion Charts Api",
       text: "It's amazing app Search Github User.Login, Logout functionalities & Mobile responsive. User can be search all github users details. ",
-        technology: "Technology Used: HTML, CSS, Material-UI, React,Context Api, React Router, Chart.js",
-        projectLink:
-        "https://react-github-user-search-app.netlify.app",
+      technology:
+        "Technology Used: HTML, CSS, Material-UI, React,Context Api, React Router, Chart.js",
+      projectLink: "https://react-github-user-search-app.netlify.app",
       githubLink: "https://github.com/kawsar007/Search-Github-User",
     },
     {
       id: 2,
+      image: SuperBike,
+      icon: <FaGithub />,
+      heading: "Super Bike Website",
+      text: "This is MERN Stack web application Mobile responsive design and User Registation Login system.",
+      technology:
+        "Technology Used: React Js, Bootstrap, Node, Express, MongoDB etc.",
+      projectLink: "https://superbike-2e664.web.app/",
+      githubLink: "https://github.com/kawsar007/Super-Bike-Client-Side",
+    },
+    {
+      id: 3,
       image: covid1,
       icon: <FaGithub />,
       heading: "Covid 19 Tracker",
       // text:"Covid 19 Tracker using React Js and Firebase. Covid 19 Affect update information",
       text: "Features: Worldwide and country wise daily and total coronavirus cases, recovered,deaths showing with heat map and chart.",
       technology: "Technology Used: HTML, CSS, Material-UI, React, Chart.js",
-     
-      projectLink:"https://5fd745c8358b5a0bc65f5f62--zealous-visvesvaraya-11d74f.netlify.app/",
+
+      projectLink:
+        "https://5fd745c8358b5a0bc65f5f62--zealous-visvesvaraya-11d74f.netlify.app/",
       githubLink: "https://github.com/kawsar007/COVID_19_Tracker",
     },
     {
-      id: 3,
+      id: 4,
       image: landing1,
       icon: <FaGithub />,
       heading: "Startup Landing Project",
-      text:
-        "This is Startup Company Landing Page, Business Website fully mobile responsive design.", 
-        technology: "Technology Used: HTML, CSS, Next js, React, theme-ui, react-scroll, react-multi-carousel etc.",
+      text: "This is Startup Company Landing Page, Business Website fully mobile responsive design.",
+      technology:
+        "Technology Used: HTML, CSS, Next js, React, theme-ui, react-scroll, react-multi-carousel etc.",
       projectLink: "https://startup-landing-lovat.vercel.app/",
-      githubLink: "https://github.com/kawsar007/Next-Js-Landing-Page"
+      githubLink: "https://github.com/kawsar007/Next-Js-Landing-Page",
     },
     {
-      id: 4,
+      id: 5,
       image: Starware,
       icon: <FaGithub />,
       heading: "Starware API Build",
-      text:
-        "Starwars API Build website Design using NextJs Static Site Generate(SSG) & Dynamic Routes.",
-        technology: "Technology Used: HTML, CSS, Next js, Styled Components.",
-        projectLink:
+      text: "Starwars API Build website Design using NextJs Static Site Generate(SSG) & Dynamic Routes.",
+      technology: "Technology Used: HTML, CSS, Next js, Styled Components.",
+      projectLink:
         "https://starwars-app-tau.vercel.app/?fbclid=IwAR29Hn9_-M68wGnidXghEiUSL0Z3B5_1ol7bbZ8p4YAe6yh_KQlTqm7Esxc",
       githubLink: "https://github.com/kawsar007/Starwars-app",
     },
     {
-      id: 5,
+      id: 6,
       image: netflix1,
       icon: <FaGithub />,
       heading: "Netflix Clone",
-      text:
-        "This is awesome Netflix Clone App using realtime database TMDB API. User can watching movie trailer.",
-        technology: "Technology Used: HTML, CSS, React Js, CSS Flexbox etc.",
+      text: "This is awesome Netflix Clone App using realtime database TMDB API. User can watching movie trailer.",
+      technology: "Technology Used: HTML, CSS, React Js, CSS Flexbox etc.",
       projectLink:
         "https://netflix-clone-cfa62.web.app/?fbclid=IwAR1d217QZ-Qk5coXi1qo7-WMOCwpQfsGMsy9dXTnh8Xeicup88KNL_97A7c",
       githubLink: "https://github.com/kawsar007/netflix-clone",
     },
-    {
-      id: 6,
-      image: commerce1,
-      icon: <FaGithub />,
-      heading: "React E-Commerce",
-      text:
-        "This is fully functional eCommerce web application Mobile responsive design and payment system", 
-        technology: "Technology Used: HTML, CSS, React Js, Material Ui, Commerce Js etc.",
-      projectLink: "https://commercejsstor.netlify.app/",
-      githubLink: "https://github.com/kawsar007/React-E-Commerce"
-    },
+    // {
+    //   id: 6,
+    //   image: commerce1,
+    //   icon: <FaGithub />,
+    //   heading: "React E-Commerce",
+    //   text:
+    //     "This is fully functional eCommerce web application Mobile responsive design and payment system",
+    //     technology: "Technology Used: HTML, CSS, React Js, Material Ui, Commerce Js etc.",
+    //   projectLink: "https://commercejsstor.netlify.app/",
+    //   githubLink: "https://github.com/kawsar007/React-E-Commerce"
+    // },
   ]);
 
   return (
@@ -96,15 +106,25 @@ function Project() {
           {state.map((info) => (
             <div className="col-4">
               <div className="project_box">
-                <div className="image"><img style={{ width: "100%", height: "100%"}} src={info.image} alt="logo"/></div>
-                <div className="projectContent">
-                <div className="project_box-header">{info.heading}</div>
-                <p className="project_box-p"> {info.text} </p>
-                <p>{info.technology}</p>
-                <div className="iconBtn">
-                    <a href={info.projectLink} className="liveDemo">Live Demo</a>
-                    <a href={info.githubLink} className="iconGit">{info.icon}</a>
+                <div className="image">
+                  <img
+                    style={{ width: "100%", height: "100%" }}
+                    src={info.image}
+                    alt="logo"
+                  />
                 </div>
+                <div className="projectContent">
+                  <div className="project_box-header">{info.heading}</div>
+                  <p className="project_box-p"> {info.text} </p>
+                  <p>{info.technology}</p>
+                  <div className="iconBtn">
+                    <a href={info.projectLink} className="liveDemo">
+                      Live Demo
+                    </a>
+                    <a href={info.githubLink} className="iconGit">
+                      {info.icon}
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
